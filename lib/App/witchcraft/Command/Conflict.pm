61885639c78a85b9ff4120f8d8ea97273c71423d
mudler <mudler@dark-lab.net>
2014-09-15 19:43:44 +0200

    fixup send_report only if there are packages to remove

--- lib/App/witchcraft/Command/Conflict.pm
+++ lib/App/witchcraft/Command/Conflict.pm
@@ -65,7 +65,8 @@ sub run {
     info "Those are the packages that are already in other repository: ";
     notice "\t$_" for @to_remove;
     return if !$self->{delete};
-    send_report( "[Conflict] Removing those packages ",@to_remove );
+    send_report( "[Conflict] Removing those packages ", @to_remove )
+        if @to_remove > 0;
     log_command("eit remove --quick --nodeps --from $overlay $_ ")
         for @to_remove;
 }
