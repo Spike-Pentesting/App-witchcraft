327d45483a38207a7fd53dfb5d0444e5c751ff4c
mudler <mudler@dark-lab.net>
2014-09-16 11:27:22 +0200

    splitting util helpers by distribution

---
+++ lib/App/witchcraft/Utils/Gentoo.pm
@@ -0,0 +1,9 @@
+package App::witchcraft::Utils::Gentoo;
+use base qw(Exporter);
+use  App::witchcraft::Utils::Base (@App::witchcraft::Utils::Base::EXPORT,@App::witchcraft::Utils::Base::EXPORT_OK);
+our @EXPORT = (@App::witchcraft::Utils::Base::EXPORT);
+our @EXPORT_OK =  (@App::witchcraft::Utils::Base::EXPORT_OK);
+#here functs can be overloaded.
+
+
+1;
