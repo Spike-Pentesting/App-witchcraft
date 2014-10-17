422215f68198dfde80bc3d2e2014af7e0641ad83
mudler <mudler@dark-lab.net>
2014-10-17 11:20:44 +0200

    attribute to allow tracking the build

--- lib/App/witchcraft/Build.pm
+++ lib/App/witchcraft/Build.pm
@@ -1,26 +1,29 @@
 package App::witchcraft::Build;
 use Deeme::Obj -base;
-use App::witchcraft::Utils qw(build_processed build_processed_manually);
-has [qw(packages id)];
-has 'manual' => 0;
+use App::witchcraft::Utils qw(build_processed build_processed_manually emit);
+has [qw(packages id args )];
+has 'manual'      => 0;
+has 'track_build' => 0;
 
 sub build {
-    App::witchcraft->instance->emit(
+    emit(
         "packages.build" => (
             (   ( ref $_[0]->packages ) ? @{ $_[0]->packages }
                 : $_[0]->packages
             ),
-            ( $self->manual == 1 )
+            ( $_[0]->track_build == 0 ) ? sub { }
+            : ( $_[0]->manual == 1 )
             ? sub { build_processed_manually( $_[0]->id ) }
             : sub { build_processed( $_[0]->id ) }            # on success
             ,
-            $_[0]->id
+            $_[0]->args,
+            $_[0]->id                                         #commit id
         )
     );
 }
 
 sub test {
-    App::witchcraft->instance->emit(
+    emit(
         "packages.test" => (
             ( ref $_[0]->packages )
             ? @{ $_[0]->packages }
