dc6afa91d0c179004435ac25269ebf34c7502545
mudler <mudler@dark-lab.net>
2014-10-26 20:35:26 +0100

    [Sync] splitting utils

--- lib/App/witchcraft/Command/Sync.pm
+++ lib/App/witchcraft/Command/Sync.pm
@@ -1,8 +1,7 @@
 package App::witchcraft::Command::Sync;
 
 use base qw(App::witchcraft::Command);
-use App::witchcraft::Utils;
-use App::witchcraft::Utils qw(send_report stage index_sync);
+use App::witchcraft::Utils qw(info error notice draw_up_line draw_down_line send_report stage index_sync);
 
 use warnings;
 use strict;
