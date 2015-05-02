5944940fb2ff5e55e21c6cea34853bb882833108
mudler <mudler@dark-lab.net>
2015-05-02 23:51:47 +0200

    commenting index_sync during stage changes

--- lib/App/witchcraft/Plugin/Git.pm
+++ lib/App/witchcraft/Plugin/Git.pm
@@ -41,7 +41,7 @@ sub register {
             return ( 1, undef ) if ( @Atoms == 0 );
             my $cwd    = cwd();
             my $return = 1;
-            emit "index_sync";
+            #emit "index_sync";
             foreach my $atom (@Atoms) {
                 eval { git::add $atom; };
                 if ($@) {
